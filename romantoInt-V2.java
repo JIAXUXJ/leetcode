5638ad4645a11b410cdfab71ad425818388fdc26
JIAXUXJ <xj.joye@outlook.com>
2018-12-06 18:45:26 -0800

    leetcode 13 java version2 42ms

---
+++ romantoInt-V2.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+class Solution {
+    private HashMap<Character, Integer> romanChar = new HashMap<Character, Integer>();
+        {
+            romanChar.put('I', 1);
+            romanChar.put('V', 5);
+            romanChar.put('X', 10);
+            romanChar.put('L', 50);
+            romanChar.put('C', 100);
+            romanChar.put('D', 500);
+            romanChar.put('M', 1000);
+        }
+    public int romanToInt(String s) {
+        
+        int last = 0;
+        int res = 0;
+        for(int i = (s.length() - 1); i >= 0; i--){
+            if(romanChar.get(s.charAt(i)) < last && i < s.length() - 1){
+                res -= romanChar.get(s.charAt(i));
+            }else{
+                res += romanChar.get(s.charAt(i));
+            }
+            last = romanChar.get(s.charAt(i));
+        }
+        return res;
+    }
+}
