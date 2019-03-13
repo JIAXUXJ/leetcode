66117fd13b4d24c458690f4ecbbd8419e7d41968
JIAXUXJ <xj.joye@outlook.com>
2019-03-13 09:02:59 -0700

    python solu ac p2

--- dup_sortArr.py
+++ dup_sortArr.py
@@ -1,3 +1,16 @@
+# class Solution(object):
+#     def removeDuplicates(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         index = 1
+#         for i in range(len(nums)):
+#             if i+1 < len(nums) and nums[i+1] != nums[i]:
+#                 nums[index] = nums[i+1]
+#                 index += 1
+#         nums = nums[:index]
+
 class Solution(object):
     def removeDuplicates(self, nums):
         """
@@ -9,4 +22,6 @@ class Solution(object):
             if i+1 < len(nums) and nums[i+1] != nums[i]:
                 nums[index] = nums[i+1]
                 index += 1
+        print(nums[:index])
         nums = nums[:index]
+        return len(nums)
