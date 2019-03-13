2d4b74e8d0622177f032c067b9f13ebadab349f3
JIAXUXJ <xj.joye@outlook.com>
2019-03-12 23:04:17 -0700

    python solu with error of last index

---
+++ dup_sortArr.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        index = 1
+        for i in range(len(nums)):
+            if i+1 < len(nums) and nums[i+1] != nums[i]:
+                nums[index] = nums[i+1]
+                index += 1
+        nums = nums[:index]
