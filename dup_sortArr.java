12bab9fa89530dd1aeee407420aa1db8b2020586
JIAXUXJ <xj.joye@outlook.com>
2019-03-20 13:59:10 -0700

    problem fixed, since it requires o(1) memory

--- dup_sortArr.java
+++ dup_sortArr.java
@@ -1,22 +1,33 @@
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         int len = nums.length;
+//         int prev;
+//         int res = 0;
+//         if(len > 0){
+//             prev = nums[0];
+//             res++;
+//         }else{
+//             return res;
+//         }
+//         for(int i = 1; i < len; i++){
+            
+//             if(nums[i] != prev){
+//                 res++;
+//             }
+//             prev = nums[i];
+//         }
+        
+//         return res;
+//     }
+// }
+
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int len = nums.length;
-        int prev;
-        int res = 0;
-        if(len > 0){
-            prev = nums[0];
-            res++;
-        }else{
-            return res;
+        int n = nums.length;
+        int res = 1;
+        for(int i = 1; i<n; i++){
+            if(nums[i] != nums[i-1]) nums[res++] = nums[i];
         }
-        for(int i = 1; i < len; i++){
-            
-            if(nums[i] != prev){
-                res++;
-            }
-            prev = nums[i];
-        }
-        
         return res;
     }
 }
