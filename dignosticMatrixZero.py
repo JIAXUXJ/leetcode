f773c5a990877c51dfb41090d79005c91d57a086
JIAXUXJ <xj.joye@outlook.com>
2019-03-24 22:56:43 -0700

    .

--- dignosticMatrixZero.py
+++ dignosticMatrixZero.py
@@ -36,3 +36,57 @@ if __name__ == "__main__":
 	makediagonalzero(mat, n, m) 
 
 # This code is contributed by ChitraNayal 
+
+// Java program to change value of 
+// diagonal elements of a matrix to 0. 
+import java.io.*; 
+
+class GFG { 
+	public static void main (String[] args) { 
+		int m[][] = {{ 2, 1, 7 }, 
+					{ 3, 7, 2 }, 
+					{ 5, 4, 9 }}; 
+		int row = 3, col = 3; 
+		GFG.diagonalMat(row,col,m); 
+	} 
+	// method to replace the diagonal matrix with zeros 
+	public static void diagonalMat(int row, int col, int m[][]){ 
+		
+		// l is the left iterator which is 
+		// iterationg from 0 to col-1[4] here 
+		//k is the right iterator which is 
+		// iterating from col-1 to 0 
+		int i =0,l=0,k=col-1; 
+		
+		// i used to iterate over rows of the matrix 
+		while(i<row){ 
+			int j=0; 
+			// condition to check if it is 
+			// the centre of the matrix 
+			if(l==k){ 
+				m[l][k] = 0; 
+				l++; 
+				k--; 
+			} 
+			//otherwize the diagonal will be equivalaent to l or k 
+			//increment l because l is traversing from left 
+			//to right and decrement k for vice-cersa 
+			else{ 
+				m[i][l] = 0; 
+				l++; 
+				m[i][k]=0; 
+				k--; 
+			} 
+			// print every element after replacing from the column 
+			while(j<col){ 
+				System.out.print(" "+ m[i][j]); 
+				j++; 
+			} 
+			i++; 
+			System.out.println(); 
+		} 
+		
+	} 
+
+} 
+
