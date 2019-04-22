adc8de37d1ca4476d14438747588962cc6dbc424
JIAXUXJ <xj.joye@outlook.com>
2019-04-21 23:35:53 -0700

    807 init

--- 807.java
+++ 807.java
@@ -1,5 +1,11 @@
 class Solution {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
-        
+		       
     }
 }
+
+__|_9__4__8__7__
+8 | 8, 4, 8, 7
+7 | 7, 4, 7, 7
+9 | 9, 4, 8, 7
+3 | 3, 3, 3, 3
