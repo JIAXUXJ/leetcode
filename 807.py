d3ae7ea138ff3720adecaaa6d2b60af140331964
JIAXUXJ <xj.joye@outlook.com>
2019-04-23 16:42:00 -0700

    807 init

--- 807.py
+++ 807.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        New = [[0] * len(grid[0]) for _ in range(len(grid))] 
-        top = [max(grid[rows][cols] for rows in range(len(grid))) for cols in range(len(grid[0]))]
-        left = [max(grid[rows][cols] for cols in range(len(grid[0]))) for rows in range(len(grid))]
-        for row, row_max in enumerate(left):
-            for col, col_max in enumerate(top):
-                New[row][col] = min(row_max, col_max)
-        return sum(New[row][col] - grid[row][col] for row in range(len(left)) for col in range(len(top)))
+        horizontal, vertical = list(), list()
+        for i in range(len(grid)):
+            horizontal_max_value = grid[i][0]
+            for j in range(len(grid)):
+                if(grid[i][j] >= horizontal_max_value):
+                    horizontal_max_value = grid[i][j]
+            horizontal.append(horizontal_max_value)
