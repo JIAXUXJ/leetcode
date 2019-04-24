5eb94f59e29fc1a9732fbdba1511d8410bf61627
JIAXUXJ <xj.joye@outlook.com>
2019-04-23 17:14:19 -0700

    807 py

--- 807.py
+++ 807.py
@@ -11,3 +11,21 @@ class Solution(object):
                 if(grid[i][j] >= horizontal_max_value):
                     horizontal_max_value = grid[i][j]
             horizontal.append(horizontal_max_value)
+            
+        for i in range(len(grid)):
+            vertical_max_value = grid[0][i]
+            for j in range(len(grid)):
+                if(grid[j][i] >= vertical_max_value):
+                    vertical_max_value = grid[j][i]
+            vertical.append(vertical_max_value)
+        
+        total, new  =  0, 0
+        for i in range(len(grid)):
+            for j in range(0, len(grid)):
+                if(horizontal[i] <= vertical[j]):
+                    new = horizontal[i]
+                else:
+                    new = vertical[j]
+                total = total + (new - grid[i][j])
+
+        return total
