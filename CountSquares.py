81ba1666dc9e44b2736391788e230405c06d1959
JIAXUXJ <xj.joye@outlook.com>
2019-08-02 14:44:59 -0700

    Write a function that gets the number of squares between two numbers 1

--- CountSquares.py
+++ CountSquares.py
@@ -1,1 +1,15 @@
 # Write a function that gets the number of squares between two numbers  
+#method 1
+def CountSquares(a, b): 
+  
+    cnt = 0 # initialize result 
+  
+    # Traverse through all numbers 
+    for i in range (a, b + 1): 
+        j = 1; 
+        while j * j <= i: 
+            if j * j == i: 
+                 cnt = cnt + 1
+            j = j + 1
+        i = i + 1
+    return cnt 
